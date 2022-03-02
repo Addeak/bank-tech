a4f7172fd654807f55f144e2c806cffa43cf86fb
Adam Deakin <adeakin89@gmail.com>
2022-03-02 13:52:21 +0000

    Can deposit, withdraw and print statement.

--- bank-tech.rb
+++ bank-tech.rb
@@ -6,14 +6,26 @@ class CashMachine
     @date = "#{@time.day}/#{@time.month}/#{@time.year}"
   end
 
+  def update_statement(amount, type)
+    if type == "deposit"
+      @statement << {date: @date, credit: " ", debit: amount, balance: @balance}
+    else
+      @statement << {date: @date, credit: amount, debit: " ", balance: @balance}
+    end
+  end
+
   def deposit(amount)
     @balance += amount
-    @statement << {date: @date, deposit: amount, balance: @balance}
+    update_statement(amount, "deposit")
   end
 
   def withdraw(amount)
     @balance -= amount
+    update_statement(amount, "withdraw")
   end
 
-
+  def print_statement
+    puts " date || credit || debit || balance "
+    @statement.each { |entry| puts "#{entry[:date]} || #{entry[:credit]} || #{entry[:debit]} || #{entry[:balance]}" }
+  end
 end
